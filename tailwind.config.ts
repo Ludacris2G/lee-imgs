ba5cb644eee9cebde6e8df326f3567d67fcbf16a
Liviu <liviutoma946@gmail.com>
2024-01-20 22:54:36 +0200

    custom color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,6 +7,12 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      black: '#040D12',
+      'dark-teal': '#183D3D',
+      'lighter-teal': '#5C8374',
+      'light-teal': '#93B1A6',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -16,5 +22,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
