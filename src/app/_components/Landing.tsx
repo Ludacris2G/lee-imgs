68f8162bc78d27fc0215f9889c9cd9dae3498308
Liviu <liviutoma946@gmail.com>
2024-03-08 17:39:48 +0200

    landing component

---
+++ src/app/_components/Landing.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GetPicturesResponse } from '../../../types/types';
+
+const Landing = (props?: GetPicturesResponse | undefined) => {
+  return <div></div>;
+};
+
+export default Landing;
