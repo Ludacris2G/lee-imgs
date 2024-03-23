88ca0408c9552e527a66bbb86d8f5a9ff7fa349b
Liviu <liviutoma946@gmail.com>
2024-03-23 14:46:02 +0200

    window resizing listener, fixes, conditionals

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -9,6 +9,8 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const [isNarrowScreen, setIsNarrowScreen] = useState(false);
+
   const handleCopyEmail = () => {
     const email = 'lee.imgsx@gmail.com';
     navigator.clipboard
@@ -31,37 +33,53 @@ function Navbar() {
     };
     window.addEventListener('scroll', handleScroll);
 
+    const resizeWindow = () => {
+      setIsNarrowScreen(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', resizeWindow);
+
+    resizeWindow();
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  useEffect(() => {}, []);
+
   return (
     <div
       className={`flex justify-between ${
-        scrolled ? 'bg-dark-teal' : 'bg-transparent'
+        scrolled ? 'bg-white' : 'bg-transparent'
       } h-14 items-center font-bold font-mono w-full fixed ease-linear duration-500`}
     >
       {/* SOCIAL ICONS */}
-      <div className='flex space-x-2 pl-4 text-black'>
-        <a target='_blank' href='https://www.instagram.com/lee.imgs/'>
-          <InstagramIcon />
-        </a>
-        <a href='#' onClick={() => handleCopyEmail()}>
-          <EmailIcon />
-        </a>
-        <a target='_blank' href='https://ludacris2g.github.io/'>
-          <CodeIcon />
-        </a>
-        <a
-          target='_blank'
-          href='https://www.youtube.com/channel/UCNA0BgcHQbIHaCXo6Hb5p4w'
-        >
-          <YouTubeIcon />
-        </a>
-      </div>
+      {!isNarrowScreen && (
+        <div className='flex space-x-2 pl-4 text-black'>
+          <a target='_blank' href='https://www.instagram.com/lee.imgs/'>
+            <InstagramIcon />
+          </a>
+          <a href='#' onClick={() => handleCopyEmail()}>
+            <EmailIcon />
+          </a>
+          <a target='_blank' href='https://ludacris2g.github.io/'>
+            <CodeIcon />
+          </a>
+          <a
+            target='_blank'
+            href='https://www.youtube.com/channel/UCNA0BgcHQbIHaCXo6Hb5p4w'
+          >
+            <YouTubeIcon />
+          </a>
+        </div>
+      )}
       {/* LOGO - NAME */}
-      <div className='flex align-middle text-[1.7rem] ml-[-68px] text-black'>
+      <div
+        className={`flex align-middle text-[1.7rem] ${
+          !isNarrowScreen ? 'ml-[-68px]' : 'ml-2'
+        } text-black`}
+      >
         <h1 className='tracking-widest'>Lee</h1>
         <CameraIcon className='scale-150 mx-2 my-auto' />
         <h1 className='ml-[-2px] tracking-widest'>imgs</h1>
