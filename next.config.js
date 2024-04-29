f54273ea355f4e5e583163a7764c810f49164b2f
Liviu <liviutoma946@gmail.com>
2024-04-29 20:41:20 +0300

    error fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['nrrriqbsrzxkhtjbbxsf.supabase.co'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
