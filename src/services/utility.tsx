74bd88c6850cc314677b891b22868379e77c5b2b
Liviu <liviutoma946@gmail.com>
2024-04-29 20:40:09 +0300

    picture architecture

--- src/services/utility.tsx
+++ src/services/utility.tsx
@@ -1,5 +1,5 @@
 import { Database } from '../../types/supabase';
-import { createClient, QueryData } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 import { GetPicturesResponse } from '../../types/types';
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -10,13 +10,30 @@ export const getPictures = async (): Promise<
   GetPicturesResponse | undefined
 > => {
   try {
-    const { data, error } = await supabase.storage
+    let photos;
+
+    const { data: section1, error } = await supabase.storage
       .from('landing')
-      .list('landing', {
-        limit: 100,
+      .list('landing-gallery', {
+        limit: 101,
         offset: 0,
         sortBy: { column: 'name', order: 'asc' },
       });
+    if (error) console.log(error);
+
+    if (section1) {
+      photos = section1.map((item: any) => ({
+        src:
+          'https://nrrriqbsrzxkhtjbbxsf.supabase.co/storage/v1/object/public/landing/landing-gallery/' +
+          item.name,
+        width: 1,
+        height: 1,
+      }));
+    }
+    const data: any = {
+      section1: photos,
+    };
+
     if (data) {
       return { data };
     }
