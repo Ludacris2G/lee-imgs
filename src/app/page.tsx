17b4a4d3f5d9d7239f779d88906abeae4c34648d
Liviu <liviutoma946@gmail.com>
2024-04-29 20:39:39 +0300

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ export default async function Home() {
   const pictures = await getPictures();
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-light-teal'>
-      <Landing {...pictures} />
-    </main>
+    <>
+      <main className='flex min-h-screen flex-col items-center justify-between bg-light-teal'>
+        <Landing {...pictures} />
+      </main>
+    </>
   );
 }
