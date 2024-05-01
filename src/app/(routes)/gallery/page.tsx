849198fa77da41698bc64545d3df1b29bfe6f0b9
Liviu <liviutoma946@gmail.com>
2024-05-01 23:02:31 +0300

    gallery page

---
+++ src/app/(routes)/gallery/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <div className='pt-14 bg-light-teal'>
+      <section></section>
+      <section></section>
+      <section></section>
+    </div>
+  );
+};
+
+export default Page;
